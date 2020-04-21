c163682998d0e71ac2d9d41cab3b09613e31fd3a
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-21 21:02:42 +1000

    each function

--- ListsAndRecursion-5/excercise.exs
+++ ListsAndRecursion-5/excercise.exs
@@ -1,4 +1,10 @@
 defmodule MyList do
   def all?([], _), do: true
   def all?([head | tail], func), do: !!func.(head) && all?(tail, func)
+
+  def each([], _), do: :ok
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
 end
